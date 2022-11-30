78447827f07fe83f52ada883a6dd08474ebe2eae
youssefjn <youssefjneyah@gmail.com>
2022-11-30 14:10:38 +0100

    Added authentication

--- Spring-Boot-Project/src/main/java/spring/boot/yj/security/WebSecurityConfig.java
+++ Spring-Boot-Project/src/main/java/spring/boot/yj/security/WebSecurityConfig.java
@@ -4,15 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
 public class WebSecurityConfig {
+private JwtRequestFilter jwtRequestFilter;
 
 	
+	public WebSecurityConfig(JwtRequestFilter jwtRequestFilter) {
+
+	this.jwtRequestFilter = jwtRequestFilter;
+}
+
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	
 		http.csrf().disable().cors().disable();
-		http.authorizeHttpRequests().anyRequest().permitAll();
+		http.addFilterBefore(jwtRequestFilter,AuthorizationFilter.class);
+		http.authorizeHttpRequests()
+		.antMatchers("/product","/auth/register","/auth/login", "/auth/verify").permitAll()
+		.anyRequest().authenticated();
 		return http.build();
 	}
 	
